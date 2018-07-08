9a713de811263c659df1a74d9ed766d48857cc03
badojo <pdj10211@gmail.com>
2018-07-08 21:05:16 +0900

    first survey code

--- contracts/Survey.sol
+++ contracts/Survey.sol
@@ -6,5 +6,63 @@ contract Survey {
     mapping (address => mapping (uint => string)) public answer;
 
     constructor() public {
+
     }
+
+    function addQuestion(string _question) public returns(uint256) {
+      uint idx = question.push(_question) - 1;
+      return idx;
+    }
+
+    function addChoice(uint idx, string _choice1) public {
+      choice[idx].push(_choice1);
+    }
+
+    function addChoice(
+      uint idx,
+      string _choice1,
+      string _choice2
+    )
+      public
+    {
+      choice[idx].push(_choice1);
+      choice[idx].push(_choice2);
+    }
+
+    function addChoice(
+      uint idx,
+      string _choice1,
+      string _choice2,
+      string _choice3
+    )
+      public
+    {
+      choice[idx].push(_choice1);
+      choice[idx].push(_choice2);
+      choice[idx].push(_choice3);
+    }
+
+    function addChoice(
+      uint idx,
+      string _choice1,
+      string _choice2,
+      string _choice3,
+      string _choice4
+    )
+      public
+    {
+      choice[idx].push(_choice1);
+      choice[idx].push(_choice2);
+      choice[idx].push(_choice3);
+      choice[idx].push(_choice4);
+    }
+
+    // 답변 등록
+
+    // 전체 내용 출력
+    function getSurvey() view public returns(string){
+
+      return "";
+    }
+
 }
