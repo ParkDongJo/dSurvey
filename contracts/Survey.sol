c2a964e1b21e532702755168a55f8b82337cead7
modolee <lsi1318@gmail.com>
2018-07-09 21:35:05 +0900

    오버로딩 함수 제거 후 string array를 파라미터로 받는 방식으로 변경

--- contracts/Survey.sol
+++ contracts/Survey.sol
@@ -1,68 +1,29 @@
 pragma solidity ^0.4.24;
+pragma experimental ABIEncoderV2;
 
 contract Survey {
-    string[]  public question;
-    mapping (uint => string[]) public choice;
-    mapping (address => mapping (uint => string)) public answer;
 
-    constructor() public {
+  string[]  public question;
+  mapping (uint => string[]) public choice;
+  mapping (address => mapping (uint => string)) public answer;
 
+  constructor() public {
+
+  }
+
+  function addQuestionAndChoice(string _question, string[] _choices) public {
+    uint256 idx = question.push(_question) - 1;
+    for (uint256 i = 0; i < _choices.length; i++) {
+      choice[idx].push(_choices[i]);
     }
+  }
 
-    function addQuestion(string _question) public returns(uint256) {
-      uint idx = question.push(_question) - 1;
-      return idx;
-    }
+  // 답변 등록
 
-    function addChoice(uint idx, string _choice1) public {
-      choice[idx].push(_choice1);
-    }
+  // 전체 내용 출력
+  function getSurvey() view public returns(string){
 
-    function addChoice(
-      uint idx,
-      string _choice1,
-      string _choice2
-    )
-      public
-    {
-      choice[idx].push(_choice1);
-      choice[idx].push(_choice2);
-    }
-
-    function addChoice(
-      uint idx,
-      string _choice1,
-      string _choice2,
-      string _choice3
-    )
-      public
-    {
-      choice[idx].push(_choice1);
-      choice[idx].push(_choice2);
-      choice[idx].push(_choice3);
-    }
-
-    function addChoice(
-      uint idx,
-      string _choice1,
-      string _choice2,
-      string _choice3,
-      string _choice4
-    )
-      public
-    {
-      choice[idx].push(_choice1);
-      choice[idx].push(_choice2);
-      choice[idx].push(_choice3);
-      choice[idx].push(_choice4);
-    }
-
-    // 답변 등록
-
-    // 전체 내용 출력
-    function getSurvey() view public returns(string){
-
-      return "";
-    }
+    return "";
+  }
 
 }
