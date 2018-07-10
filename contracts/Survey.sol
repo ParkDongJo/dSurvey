faf135d3c3faa259ddd1b450ffbf3d8d77831d1a
modolee <lsi1318@gmail.com>
2018-07-10 20:33:46 +0900

    에러 제거, 답변을 등록한 사용자와 등록한 답변 목록을 받아 올 수 있게 수정

--- contracts/Survey.sol
+++ contracts/Survey.sol
@@ -5,52 +5,52 @@ import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/ownershi
 
 contract Survey is Ownable {
 
-  struct userAnswer {
-    address user;
-    string answer;
-  }
+  string[] internal question; // 질문 목록
+  mapping (uint => string[]) internal choice; // 질문 별 보기 목록
+  mapping (address => mapping (uint => string)) internal answer; // 사용자의 질문 별 답변
+  address[] internal answeredUsers; // 답변을 등록한 사용자 목록
+  mapping (uint => string[]) internal answersPerQuestion; // 질문 별 답변 목록
 
-  string[]  public question;
-  mapping (uint => string[]) public choice;
-  mapping (address => mapping (uint => string)) public answer;
-  mapping (uint => userAnswer[]) public userAnswers;
-  
+  // 생성자
+  // SurveyController가 기본 owner로 설정되어서 요청자로 owner를 변경함
+  // 이걸 실행하지 않고 요청자가 owner가 되게 하려면 controller에서 호출 할 때
+  // delegateCall로 생성자를 호출해야 함
   constructor(address _newOwner) public {
     transferOwnership(_newOwner);
   }
 
   // 질문과 선택지 입력
-  function addQuestionAndChoice(string _question, string[] _choices) public {
+  function addQuestionAndChoices(string _question, string[] _choices) public {
     uint256 idx = question.push(_question) - 1;
-    for (uint256 i = 0; i < _choices.length; i++) {
+    for (uint i = 0; i < _choices.length; i++) {
       choice[idx].push(_choices[i]);
     }
   }
 
   // 전체 내용 출력
-  function getSurvey(uint _idx) view public returns(string, string[]){
+  function getQuestionAndChoices(uint _idx) view public returns(string, string[]){
     return (question[_idx], choice[_idx]);
   }
 
-  // 질문 당 각 유저와 대답 출력
-  function getUserAnswer(uint _idx) view public returns(address[], string[]) {
-    userAnswer[] list = userAnswers[_idx];
-    address[] users;
-    string[] answers;
-
-    for (uint256 i = 0; i < list.length; i++) {
-      users.push(list[i].user);
-      answers.push(list[i].answer);
+  // 답변 등록
+  function setAnswers(string[] _answers) public {
+    // 사용자 별 답변 목록 및 질문 별 답변 목록에 추가
+    for (uint i = 0; i < _answers.length; i++) {
+      answer[msg.sender][i] = _answers[i];
+      answersPerQuestion[i].push(_answers[i]);
     }
-
-    return (users, answers);
+    // 답변 한 사용자 목록에 추가
+    answeredUsers.push(msg.sender);
   }
 
-  // 답변 등록
-  function setAnswer(string[] _answers) public {
-    for (uint256 i = 0; i < _answers.length; i++) {
-      answer[msg.sender][i] = _answers[i];
-    }
+  // 답변을 등록한 사용자 목록
+  function getAnsweredUsers() view public returns(address[]) {
+    return answeredUsers;
+  }
+
+  // 질문 당 사용자들이 등록한 답변 목록
+  function getAnswersPerQuestion(uint _idx) view public returns(string[]) {
+    return answersPerQuestion[_idx];
   }
 
 }
