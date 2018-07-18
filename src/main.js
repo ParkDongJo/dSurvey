845745fbefa61a42b8e413c24df715c7864223d6
badojo <pdj10211@gmail.com>
2018-07-18 22:34:03 +0900

    directory 구조 변경

--- src/main.js
+++ src/main.js
@@ -1,33 +1,16 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './pages/App.vue'
-import Web3 from 'web3'
-import router from './router/index'
-import store from './store/index'
-import { sync } from 'vuex-router-sync'
-
-sync(store, router)
+import App from './App'
+import router from './router'
+import { store } from './store/'
 
 Vue.config.productionTip = false
 
-window.addEventListener('load', function () {
-  if (typeof web3 !== 'undefined') {
-    console.log('Web3 injected browser: OK.')
-    window.web3 = new Web3(window.web3.currentProvider)
-  } else {
-    console.log('Web3 injected browser: Fail. You should consider trying MetaMask.')
-    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    window.web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
-  }
-
-  /* eslint-disable no-new */
-  new Vue({
-    el: '#app',
-    store,
-    router,
-    template: '<App/>',
-    render: h => h(App)
-  })
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  components: { App },
+  template: '<App/>'
 })
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,36 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Dashboard from '../pages/Dashboard'
-import Signup from '../pages/Signup'
-import Survey from '../pages/Survey'
-import AddSurvey from '../pages/AddSurvey'
+import CasinoDapp from '@/pages/casino-dapp'
+
 
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
-  base: __dirname,
-  linkActiveClass: 'active',
   routes: [
     {
       path: '/',
-      name: 'dashboard',
-      component: Dashboard
-    },
-    {
-      path: '/signup',
-      name: 'signup',
-      component: Signup
-    },
-    {
-      path: '/survey',
-      name: 'Survey',
-      component: Survey
-    },
-    {
-      path: '/survey/add',
-      name: 'addSurvey',
-      component: AddSurvey
+      name: 'casino-dapp',
+      component: CasinoDapp
     }
   ]
 })
