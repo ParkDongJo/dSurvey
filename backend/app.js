2bb3723ce294a303bf94fe9221de611d817c276b
modolee <lsi1318@gmail.com>
2018-07-26 23:41:11 +0900

    오타 수정

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan');
 
-cons app = express();
+const app = express();
 
 app.use(morgan('combined'));
 app.use(bodyParser.json())
