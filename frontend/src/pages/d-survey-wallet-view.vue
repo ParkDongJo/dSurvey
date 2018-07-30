7d501eedb45a55537d6e4d55f0709daa0356133d
ParkDongJo <pdj10211@gmail.com>
2018-07-30 19:46:48 +0900

    wallet vue 카드리스트 추가

--- frontend/src/pages/d-survey-wallet-view.vue
+++ frontend/src/pages/d-survey-wallet-view.vue
@@ -3,6 +3,36 @@
     <app-header></app-header>
 
     <p>Token: {{ value }}</p>
+
+    <b-card-group deck>
+      <b-card header="<b>Transactional log</b>">
+        <b-list-group>
+          <b-list-group-item href="#">Cras justo odio</b-list-group-item>
+          <b-list-group-item href="#">Dapibus ac facilisis in</b-list-group-item>
+          <b-list-group-item href="#">Vestibulum at eros</b-list-group-item>
+        </b-list-group>
+        <p class="card-text mt-2">
+          Quis magna Lorem anim amet ipsum do mollit sit cillum voluptate ex
+          nulla tempor. Laborum consequat non elit enim exercitation cillum aliqua
+          consequat id aliqua. Esse ex consectetur mollit voluptate est in duis laboris
+          ad sit ipsum anim Lorem.
+        </p>
+      </b-card>
+      <b-card header="<b>Owned survey List</b>">
+        <b-list-group>
+          <b-list-group-item href="#">Cras justo odio</b-list-group-item>
+          <b-list-group-item href="#">Dapibus ac facilisis in</b-list-group-item>
+          <b-list-group-item href="#">Vestibulum at eros</b-list-group-item>
+        </b-list-group>
+        <p class="card-text mt-2">
+          Quis magna Lorem anim amet ipsum do mollit sit cillum voluptate ex
+          nulla tempor. Laborum consequat non elit enim exercitation cillum aliqua
+          consequat id aliqua. Esse ex consectetur mollit voluptate est in duis laboris
+          ad sit ipsum anim Lorem.
+        </p>
+      </b-card>
+    </b-card-group>
+
     <!--<p>-->
       <!--<b-button v-on:click="getBalance">I am a Button</b-button>-->
     <!--</p>-->
